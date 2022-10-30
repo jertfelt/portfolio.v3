d35f565046deb36c80661542b107c05e4f321df9
Tova Jertfelt <tovizitan@gmail.com>
2022-10-30 18:13:55 +0100

    kan nu hämta från firebase

--- components/api/Data.js
+++ components/api/Data.js
@@ -4,33 +4,28 @@ import { useEffect, useState } from "react"
 
 const DATA = () => {
   const [firebaseData, setFirebaseData] = useState(null)
-  console.log(firebaseData)
-
   useEffect(() => {
-    initFirebase()
     getDataFirebase()
-  , []})
+  , [getDataFirebase()]})
 
 const getDataFirebase = () => {
+  initFirebase()
 const dbRef = ref(getDatabase());
 get(child(dbRef, `/cases/`)).then((snapshot) => {
- 
   setFirebaseData(snapshot.val())
   if (snapshot.exists()) {
     setFirebaseData(snapshot.val())
-    console.log("snap", firebaseData)
   } else {
     console.log("No data available");
   }
 }).catch((error) => {
   console.error(error);
 });
-console.log("nytt", firebaseData)
-   
+
 }
 return (
   <>
-  {firebaseData}
+  {firebaseData && <div></div>}
   </>
 )
 
--- utils/db/index.js
+++ utils/db/index.js
@@ -2,6 +2,7 @@ import { getApps, initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
+
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
