066d17419d70fd354cde0c6d547877a2e109eb0b
Tova Jertfelt <tovizitan@gmail.com>
2022-11-07 12:45:16 +0100

    error getStaticProps

--- pages/cases/[id].js
+++ pages/cases/[id].js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import {Container} from "../../components/styles/Container.styled.js"
 import {flex, device} from "../../components/styles/Styles"
 import Image from "next/image";
+import { cases } from "../../data/cases.js";
 
 const Content = styled.div`
 min-height:100vh;
@@ -11,7 +12,6 @@ ${flex}
 font-family:Roboto;
 font-size:${({theme}) => theme.fontSizes.medium};
 h1{
-  margin-top:-3em;
   font-family:Arya;
   font-size:${({theme}) => theme.fontSizes.xlarge};
   color: ${({theme}) => theme.colors.lightblue};
@@ -19,15 +19,48 @@ h1{
   margin-bottom:-1rem;
 }`
 
+
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  
+  const paths = data.map(ninja => {
+    return {
+      params: {id: ninja.id.toString()}
+    }
+  })
+
+//version 2:
+// const data = cases;
+// console.log(data)
+// const paths = data.map(item => {
+//   return {
+//     params: {id: item.id.toString()}
+//   }
+// })
+// console.log("paths", paths)
+
+  return {
+    paths, 
+    fallback: false, //visar 404 om man försöker gå in på en id som inte finns
+  }
+}
+
+
+
+
 const Details = () => {
   return ( 
     <Container xlarge>
       <Content>
-      <h1>Test</h1>
+      <h1>Test </h1>
       <p>This is a test</p>
       </Content>
     </Container>
   );
 }
+
+
 
 export default Details;
