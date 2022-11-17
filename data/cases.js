3bee8f7e780127c4ebd09d04cbdbefb50075a6ba
Tova Jertfelt <tovizitan@gmail.com>
2022-11-17 18:00:02 +0100

    changed img url bug

--- data/cases.js
+++ data/cases.js
@@ -516,7 +516,7 @@ moreinfo:{
       alt:"Printscreen",
     },
     file02:{
-      url:"/img/ekobanken--screenshot2.png",
+      url:"/img/ekobanken-screenshot2.png",
       alt:"Startsidan och kod",
     },
     file03:{
