14de8d0696f567ab39a714f50b5b1e91d518bccb
Tova Jertfelt <30622818+jertfelt@users.noreply.github.com>
2023-03-01 13:12:54 +0100

    Update cases.js

--- data/cases.js
+++ data/cases.js
@@ -591,7 +591,7 @@ moreinfo:{
 },
 {
   id:12,
-  featured: false,
+  featured: true,
   tag: "TDD: React",
   title:"Westcoast Education",
   sub:"Studentarbete i React och jest",
@@ -622,7 +622,7 @@ moreinfo:{
     }
 },
 moreinfo:{
-  text1: " Vi skulle bygga en adminportal till en skola med kurser och lärare.  Jag har även jobbat med Netlify och firebase, och då migrerat från att jobba med localserver och json server. Skoluppgiften gick ut på att skriva testdrivet, här har jag jobbat med unit-testing och viss interaction testing med hjälp av Jest.",
+  text1: "Vi skulle bygga en adminportal till en skola med kurser och lärare.  Jag har även jobbat med Netlify och firebase, och då migrerat från att jobba med localserver och json server. Skoluppgiften gick ut på att skriva testdrivet, här har jag jobbat med unit-testing och viss interaction testing med hjälp av Jest.",
   text1part2:"Jag har vidare jobbat med styled components, och testat olika filstrukturer. Vidare har jag experimenterat med dark mode/ light mode och tillgänglighet.",
   text1part3: "Sidan är fortfarande under uppbyggnad och kan ha vissa buggar igång. Jag håller fortfarande på att skriva klart logiken för studenter, för att lägga till kurs som student, och för att kors-referera lärare, studenter och kurser.",
   teknik:
